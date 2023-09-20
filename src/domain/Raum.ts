b51dcae389f8ab669d4b87b498051b6411f9bffc
Marc Hinte <m.hinte@neusta.de>
2023-09-20 15:41:15 +0200

    Add domain structure

---
+++ src/domain/Raum.ts
@@ -0,0 +1,48 @@
+import {v4 as uuid} from 'uuid';
+
+import {Person} from "./Person";
+
+export class Raum {
+
+
+    public id: string;
+
+    public nummer: string;
+
+    public name: string;
+
+    private personen: Array<Person>;
+
+
+    constructor(nummer: string, name: string) {
+
+        if (!this.istNummerGueltig(nummer)) {
+
+            return null
+
+        }
+
+        this.id = uuid()
+
+        this.personen = []
+
+        this.name = name
+
+        this.nummer = nummer
+
+    }
+
+    fuegePersonHinzu(person: Person) {
+
+        this.personen.push(person)
+
+    }
+
+
+    private istNummerGueltig(nummer: string): boolean {
+
+        return nummer.length === 4 && !!isNaN(+nummer)
+
+    }
+
+}
