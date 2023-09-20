bca7b10bb6e483615ed0baa8e8ffba1d6d322129
Marc Hinte <m.hinte@neusta.de>
2023-09-20 11:47:02 +0200

    Add api/hello endpoint and start script

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 const app = express();
-const port = 3000;
 
- 
+const apiUrl: string = "/api"
+const port: number = 3000;
 
-app.get('/', (req, res) => {
+app.get(apiUrl + '/hello', (req, res) => {
   res.send('Hello World!');
 });
 
- 
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
