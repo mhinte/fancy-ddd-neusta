74e57150ecd5f0fa89ffb91b9aa5a50970e30bed
Niklas Emanuel <n.emanuel@neusta.de>
2023-09-21 13:53:11 +0200

    check if person exists when adding person to room

--- src/application/common/PersonZuRaumHinzufuegen.ts
+++ src/application/common/PersonZuRaumHinzufuegen.ts
@@ -1,13 +1,16 @@
 import { RaumRepository } from "../../domain/raum/RaumRepository";
 import { Raum, RaumId } from "../../domain/raum/Raum";
-import { PersonenId } from "../../domain/person/Person";
+import { Person, PersonenId } from "../../domain/person/Person";
+import { PersonRepository } from "../../domain/person/PersonRepository";
 
 export class PersonZuRaumHinzufuegen {
 
     raumRepository: RaumRepository
+    personRepository: PersonRepository
 
-    constructor(raumRepository: RaumRepository) {
+    constructor(raumRepository: RaumRepository, personRepository: PersonRepository) {
         this.raumRepository = raumRepository
+        this.personRepository = personRepository
     }
 
     ausfuehren(raumId: RaumId, personId: PersonenId): boolean {
@@ -16,7 +19,10 @@ export class PersonZuRaumHinzufuegen {
             throw new Error(`Der Raum existiert nicht.`)
         }
 
-        // TODO chekc if person exists
+        const person: Person = this.personRepository.finde(personId)
+        if (!person) {
+            throw new Error(`Die Person existiert nicht.`)
+        }
 
         if (raum.hatPerson(personId)) {
             throw new Error(`Die Person mit der Id ${personId} ist bereits in dem Raum ${raum.name}.`)
